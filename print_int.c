48b9e58290c60055c13414d26babfaa6b5e599e0
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2023-09-19 22:12:19 +0100

    fix: print_int return lenght

--- print_int.c
+++ print_int.c
@@ -13,18 +13,21 @@ int print_int(int n)
 	int i, len;
 	char c[10];
 
+	len = 0;
+
 	if (n == -2147483648)
 	{
 		_putchar('-');
 		_putchar('2');
 		n = 147483648;
-		len = 1440;
+		len += 2;
 	}
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * (-1);
+		len += 1;
 	}
 	if (n == 0)
 	{
@@ -37,7 +40,7 @@ int print_int(int n)
 		n = n / 10;
 		i++;
 	}
-	len = i;
+	len += i;
 	i--;
 	while (i >= 0)
 	{
