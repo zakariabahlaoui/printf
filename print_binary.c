d0b46d18de09b0b7e969a0ea239b2261f64bbe6f
balmed <iwymed@gamil.com>
2023-09-19 11:44:40 -0700

    modify print_binary

--- print_binary.c
+++ print_binary.c
@@ -4,27 +4,30 @@
  * print_binary - print the number  octal.
  * @num: numbre integer
  *
- * return: value binary.
+ * return: return lenght.
  */
- int print_binary(int num)
- {
+int print_binary(int num)
+{
 	int bits[32];
-    int i = 0;
-    if (num == 0)
-	{
-        putchar('0');
-        return;
-    }
-	
-    while (num > 0)
-	{
-        bits[i] = num % 2;
-        num /= 2;
-        i++;
-    }
+	int i = 0;
+	int len = 0;
 
-    for (int j = i - 1; j >= 0; j--)
+	if (num == 0)
 	{
-        putchar(bits[j] ? '1' : '0');
-    }
+		putchar('0');
+		return;
+	}
+	while (num > 0)
+	{
+		bits[i] = num % 2;
+		num /= 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		putchar(bits[j] ? '1' : '0');
+		len++;
+	}
+	return (len);
+
 }
