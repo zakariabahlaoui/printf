8d5bb7b022339b48ef3363ec030df9c295e98288
balmed <iwymed@gamil.com>
2023-09-20 19:25:02 -0700

    create print_rot13

---
+++ print_rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_rot13 - prints convert lowercase to uppercase and reverse.
+ * @ptr: pointer
+ *
+ * Return: Length of the printed.
+ */
+
+int print_rot13(char *ptr)
+{
+	char c;
+	int len = 0;
+
+	while (*ptr)
+	{
+		c = *ptr;
+		if (c >= 'A' && c <= 'Z')
+		{
+			c = c + ('a' - 'A');
+		}
+		else
+		{
+			c = c + ('A' - 'a');
+		}
+		ptr++;
+		putchar(c);
+		len++;
+	}
+	return (len);
+}
